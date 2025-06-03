7bf40fbc477cc52a46533db6cfee2f8458086181
Alberthzaid <alberthzaid2003@gmail.com>
2025-06-03 15:25:48 -0500

    Base main

---
+++ home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.set_page_config(page_title="Crime", page_icon="./assets/Logo-removebg.png")
+
+st.markdown("# Welcome to our project")
+st.sidebar.header("Crime Prediction")
