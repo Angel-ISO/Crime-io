72107e5695441a97d033138da27b55967a7ee921
Angel-ISO <angelgabrielorteg@gmail.com>
2025-06-03 23:42:49 -0500

    🔖 Map Version added with deepseek

--- src/Pages/GuardiaAI.py
+++ src/Pages/GuardiaAI.py
@@ -2,4 +2,3 @@ import streamlit as st
 st.set_page_config(page_title="Crime", page_icon="./assets/Logo-removebg.png")
 
 st.markdown("# Welcome to our project")
-st.sidebar.header("Crime Prediction")
